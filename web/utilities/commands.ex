c12ba464ba9fe4bda08727d95c959eec3812de32
brian <bolecki019@gmail.com>
2017-03-05 16:47:04 -0500

    fix compiler warnings

--- web/utilities/commands.ex
+++ web/utilities/commands.ex
@@ -1,36 +1,47 @@
 defmodule Volition.Commands do
-  use Phoenix.Channel
-  
-  def command(command, socket) do
-    case command do
-      "am " <> name ->
-        character = Volition.Repo.get_by(Volition.Player, name: name) |> Volition.Repo.preload(area: :nearbys)
-        if !character do
-          character = Volition.Repo.insert!(%Volition.Player{name: name, area_id: 2, gold: 0, health: 100, mana: 100}) |> Volition.Repo.preload(area: :nearbys)
-        end
-        socket = assign(socket, :player, character)
-        push socket, "new_msg", %{body: "you are " <> name}
-      "where" ->
-        if socket.assigns[:player] do
-          push socket, "new_msg", %{body: "you are in " <> socket.assigns[:player].area.name}
-        else
-          push socket, "new_msg", %{body: "to am is to place"}
-        end
-      "nearby" ->
-        if socket.assigns[:player] do
-          names = Enum.map(socket.assigns[:player].area.nearbys, fn x -> x.name end)
-          msg = Enum.join(names, ", ")
-          push socket, "new_msg", %{body: "you are near " <> msg}
-        else
-          push socket, "new_msg", %{body: "to am is to place"}
-        end
-      _ ->
-        if socket.assigns[:player] do
-          broadcast! socket, "new_msg", %{body: socket.assigns[:player].name <> ": " <> command}
-        else
-          broadcast! socket, "new_msg", %{body: "anonymous: " <> command}
-        end
+
+  def command("am " <> name, socket) do
+    character =
+      Volition.Repo.get_by(Volition.Player, name: name)
+      |> Volition.Repo.preload(area: :nearbys) ||
+      Volition.Repo.insert!(%Volition.Player{name: name, area_id: 2, gold: 0, health: 100, mana: 100})
+      |> Volition.Repo.preload(area: :nearbys)
+
+    socket = Phoenix.Socket.assign(socket, :player, character)
+    Phoenix.Channel.push socket, "new_msg", %{body: "you are " <> name}
+    {:noreply, socket}
+  end
+
+  def command("where", socket) do
+    if socket.assigns[:player] do
+      Phoenix.Channel.push socket, "new_msg", %{body: "you are in " <> socket.assigns[:player].area.name}
+    else
+      Phoenix.Channel.push socket, "new_msg", %{body: "to am is to place"}
     end
     {:noreply, socket}
   end
+
+  def command("nearby", socket) do
+    if socket.assigns[:player] do
+      names = Enum.map(socket.assigns[:player].area.nearbys, fn x -> x.name end)
+      msg = Enum.join(names, ", ")
+      Phoenix.Channel.push socket, "new_msg", %{body: "you are near " <> msg}
+    else
+      Phoenix.Channel.push socket, "new_msg", %{body: "to am is to place"}
+    end
+    {:noreply, socket}
+  end
+
+  def command(message, socket) do
+    if socket.assigns[:player] do
+      Phoenix.Channel.broadcast! socket, "new_msg", %{body: socket.assigns[:player].name <> ": " <> message}
+    else
+      Phoenix.Channel.broadcast! socket, "new_msg", %{body: "anonymous: " <> message}
+    end
+    {:noreply, socket}
+  end
+
+  # def command("", socket) do
+  #   {:noreply, socket}
+  # end
 end
